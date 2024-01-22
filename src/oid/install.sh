2053cf288c05ae6165552148200471e028e84ba8
David Khouya <dakhouya@gmail.com>
2024-01-21 20:37:41 -0500

    Remove examples and add oid

---
+++ src/oid/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+  if ! dpkg -s "$@" >/dev/null 2>&1; then
+    apt-get update -y
+    apt-get -y install --no-install-recommends "$@"
+  fi
+}
+
+# Install depot_tools
+install() {
+    local oid_src="/tmp/oid-src"
+    local oid_install="/opt/oid"
+    git clone https://github.com/OpenImageDebugger/OpenImageDebugger.git "${oid_src}"
+    cd "${oid_src}"
+    git submodule init
+    git submodule update
+
+    cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${oid_install}"
+    cmake --build build --config Release --target install -j
+    rm -rf "${oid_src}"
+}
+
+# Install dependencies
+check_packages git build-essential libpython3-dev python3-dev cmake qtbase5-dev libqt5opengl5-dev
+
+install
