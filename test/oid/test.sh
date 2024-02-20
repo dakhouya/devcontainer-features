24b12b287d4e3061d63d42938d9b99cc130957bf
David Khouya <dakhouya@gmail.com>
2024-02-19 19:25:57 -0500

    Fix tests

--- test/oid/test.sh
+++ test/oid/test.sh
@@ -8,8 +8,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "oid exist" ls -lah /opt/OpenImageDebugger
 check "oid initialisation script is available" ls -lah /opt/OpenImageDebugger/oid.py
-check "validate lldb hook is available" grep -q "command script import /path/to/OpenImageDebugger/oid.py" ${HOME}/.lldbinit
-check "validate gdb hook is available" grep -q "source /path/to/OpenImageDebugger/oid.py" ${HOME}/.gdbinit
+check "validate lldb hook is available" grep -q "command script import /opt/OpenImageDebugger/oid.py" ${HOME}/.lldbinit
+check "validate gdb hook is available" grep -q "source /opt/OpenImageDebugger/oid.py" ${HOME}/.gdbinit
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
